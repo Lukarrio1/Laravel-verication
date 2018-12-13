3c0751b1f76b6630496acf7250aa0fb715b48962
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2018-12-13 16:12:17 -0500

    jumbo fix

--- resources/views/Account/index.blade.php
+++ resources/views/Account/index.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    this is the account page..
+  <div class="jumbotron text-center">
+        <h1 class="display-4">Hello,{{ Auth::user()->name }}</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,5 @@ Dashboard
 <div class="jumbotron text-center">
         <h1 class="display-4">Hello, world!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+</div>
 @endsection
