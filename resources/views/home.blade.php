e11614eeeddc97105b247ac4b7e22ba326e7def3
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2018-12-13 16:08:34 -0500

    jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,7 @@
 Dashboard
 @endsection
 @section('content')
-this is the dash board page
+<div class="jumbotron text-center">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 @endsection
