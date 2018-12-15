eff0981e62c6946dc66c02469cca52fa5f7845d9
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2018-12-14 19:03:24 -0500

    password reset completed

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'tomenis1997@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'tomennis1997@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Xpress-Logisticsja'),
     ],
 
