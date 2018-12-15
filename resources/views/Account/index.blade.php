29cd068a1300941bb2fef680c80672945f49bc4a
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2018-12-14 21:55:34 -0500

    fix

--- resources/views/Account/index.blade.php
+++ resources/views/Account/index.blade.php
@@ -2,6 +2,19 @@
 @section('content')
   <div class="jumbotron text-center">
         <h1 class="display-4">Hello,{{ Auth::user()->name }}</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p class="lead">This is the account page..</p>
+</div>
+<div class="col-12 pl-3 pr-3 ">
+  <div class="card">
+    <div class="card-header bg-white">
+      Dashboard
+    </div>
+    <div class="card-body">
+      this is the card body.
+    </div> 
+    <div class="card-footer bg-white">
+      this is the card footer
+    </div>
+  </div>
 </div>
 @endsection
